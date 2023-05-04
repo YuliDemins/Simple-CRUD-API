0695748daf89a291ecbfc1b83d894969ea26df6d
Yulia <yudemins@mail.ru>
2023-05-04 20:31:53 +0300

    feat: add commanf multi

--- src/server.ts
+++ src/server.ts
@@ -26,66 +26,24 @@ let DB: DBtype = [
   },
 ];
 
+const args = process.argv.slice(2);
+
 const PORT = process.env.PORT || 3001;
-// const PORT = 3000;
+
 const WORKER_PORT = process.env.WORKER_PORT || 4001;
-// const server = createServer((request: IncomingMessage, response: ServerResponse) => {
-//     const { method, url } = request;
 
-//     console.log('Server request');
-
-//     switch (method) {
-//       case Methods.GET:
-//         if (url) getUsers(url, response, DB);
-//         break;
-//       case Methods.POST:
-//         if (url) setUser(url, request, response, DB);
-//         break;
-//       case Methods.PUT:
-//         if (url) changeUser(url, request, response, DB);
-//         break;
-//       case Methods.DELETE:
-//         if (url) deleteUser(url, response, DB);
-//         break;
-//       default:
-//         wrongRoute(response);
-//     }
-//   }
-// );
-
-// server.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-if (cluster.isPrimary) {
-  console.log( `Master start ${process.pid}`)
-  for (let i = 0; i < cpus().length; i++){
-    const port = +WORKER_PORT + i;
-    const worker = cluster.fork({ WORKER_PORT: port });
-  }
-}
-if (cluster.isWorker){
-//   const server = createServer((req, res:ServerResponse) => {
-// const message = `Worker start ${process.pid}`
-// res.end(message)
-//   }).listen(process.env.PORT, () => console.log(`server started ${process.env.PORT}, Worker start ${process.pid}`))
+const startServer = () => {
   const server = createServer((request: IncomingMessage, response: ServerResponse) => {
-
     const { method, url } = request;
 
-    
-    console.log(`Worker ${process.pid} received request on ${url}`);
+    console.log('Server request');
 
     switch (method) {
       case Methods.GET:
-        if (url) {
-          getUsers(url, response, DB);
-        }
+        if (url) getUsers(url, response, DB);
         break;
       case Methods.POST:
-        if (url) {
-          setUser(url, request, response, DB);
-        }
+        if (url) setUser(url, request, response, DB);
         break;
       case Methods.PUT:
         if (url) changeUser(url, request, response, DB);
@@ -99,7 +57,62 @@ if (cluster.isWorker){
   }
 );
 
-server.listen(process.env.WORKER_PORT, () => console.log(`server started ${WORKER_PORT}, Worker start ${process.pid}`))
-
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 }
 
+
+const startWorkers = () => {
+  if (cluster.isPrimary) {
+    console.log( `Master start ${process.pid}`)
+    for (let i = 0; i < cpus().length; i++){
+      const port = +WORKER_PORT + i;
+      const worker = cluster.fork({ WORKER_PORT: port });
+    }
+  }
+  if (cluster.isWorker){
+  //   const server = createServer((req, res:ServerResponse) => {
+  // const message = `Worker start ${process.pid}`
+  // res.end(message)
+  //   }).listen(process.env.PORT, () => console.log(`server started ${process.env.PORT}, Worker start ${process.pid}`))
+    const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+  
+      const { method, url } = request;
+  
+      console.log(`Worker ${process.pid} received request on ${url}`);
+  
+      switch (method) {
+        case Methods.GET:
+          if (url) {
+            getUsers(url, response, DB);
+          }
+          break;
+        case Methods.POST:
+          if (url) {
+            setUser(url, request, response, DB);
+          }
+          break;
+        case Methods.PUT:
+          if (url) changeUser(url, request, response, DB);
+          break;
+        case Methods.DELETE:
+          if (url) deleteUser(url, response, DB);
+          break;
+        default:
+          wrongRoute(response);
+      }
+    }
+  );
+  
+  server.listen(process.env.WORKER_PORT, () => console.log(`server started ${WORKER_PORT}, Worker start ${process.pid}`))
+  
+  }
+}
+
+if (args.length === 0) {
+  startServer()
+}
+else {
+  startWorkers()
+}
